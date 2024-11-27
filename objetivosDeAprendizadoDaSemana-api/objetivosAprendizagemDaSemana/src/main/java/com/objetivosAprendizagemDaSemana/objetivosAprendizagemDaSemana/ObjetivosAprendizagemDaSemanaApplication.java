f9785354ba4462802f9ad4e3e9ea81100cac7f1f
Ivan Barbosa <ivanjulio.ij@gmail.com>
2024-11-26 23:26:53 -0300

    Tarefa/atividade completa com os 3 exercícios.

---
+++ objetivosDeAprendizadoDaSemana-api/objetivosAprendizagemDaSemana/src/main/java/com/objetivosAprendizagemDaSemana/objetivosAprendizagemDaSemana/ObjetivosAprendizagemDaSemanaApplication.java
@@ -0,0 +1,13 @@
+package com.objetivosAprendizagemDaSemana.objetivosAprendizagemDaSemana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObjetivosAprendizagemDaSemanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObjetivosAprendizagemDaSemanaApplication.class, args);
+	}
+
+}
---
+++ objetivosDeAprendizadoDaSemana-api/objetivosAprendizagemDaSemana/src/test/java/com/objetivosAprendizagemDaSemana/objetivosAprendizagemDaSemana/ObjetivosAprendizagemDaSemanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.objetivosAprendizagemDaSemana.objetivosAprendizagemDaSemana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObjetivosAprendizagemDaSemanaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
